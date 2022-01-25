7e5ebeb78cb7478907cc2255390ce4127ea24d38
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-01-25 09:38:55 -0500

    whitespace

--- test/unit/test_events.py
+++ test/unit/test_events.py
@@ -511,6 +511,7 @@ class TestLazyMemoizationInCacheEvents(TestCase):
                 if 'dump' in str(e):
                     # make the class. If this throws, maybe your class didn't use Lazy when it should have
                     e = clazz(dump = Lazy.defer(lambda: counter.next()))
+
                     # assert that initializing the event with the counter
                     # did not evaluate the lazy value
                     self.assertEqual(counter.count, 0)
